fa46f4eaf5e57c34efe1fc0c4ba65df681616573
huangnauh <huanglibo2010@gmail.com>
2020-11-26 10:27:29 +0800

    some change

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
 
 # vscode
 # Add-PathVariable "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\bin" 
-$env:path +=  ";D:\software\SysinternalsSuite"
-$env:path +=  ";D:\software\Command"
+$env:path += ";D:\software\SysinternalsSuite"
+$env:path += ";D:\software\Command"
 $Env:https_proxy = "http://127.0.0.1:7890"
 # Import-Module oh-my-posh
 Import-Module PSReadLine
@@ -29,7 +29,8 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 # Change how powershell does tab completion
 # http://stackoverflow.com/questions/39221953/can-i-make-powershell-tab-complete-show-me-all-options-rather-than-picking-a-sp
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
-
+Set-PSReadlineKeyHandler -Key Ctrl+q -Function TabCompleteNext
+Set-PSReadlineKeyHandler -Key Ctrl+Shift+q -Function TabCompletePrevious
 # The built-in word movement uses character delimiters, but token based word
 # movement is also very useful - these are the bindings you'd use if you
 # prefer the token based movements bound to the normal emacs word movement
@@ -38,8 +39,10 @@ Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
 Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
 Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
 Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
-Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
-Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+j -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+k -Function SelectShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+u -Function BackwardKillLine
+Set-PSReadLineKeyHandler -Key Alt+w -Function BackwardKillWord
 
 
 
@@ -52,7 +55,7 @@ Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
 $profileDir = $PSScriptRoot;
 
 foreach ( $includeFile in ("Get-Hash", "New-RandomPassword", "ForEach-Parallel", "ConvertFrom-UnixDate", "ConvertTo-UnixDate", "ConvertFrom-Base64", "ConvertTo-Base64") ) {
-	# Unblock-File "$profileDir\Scripts\$includeFile.ps1"
+    # Unblock-File "$profileDir\Scripts\$includeFile.ps1"
     . "$profileDir\Scripts\$includeFile.ps1"
 }
 
@@ -73,7 +76,7 @@ Set-Alias open Invoke-Item
 Set-Alias .. GoBack
 
 function Get-Path {
-	($Env:Path).Split(";")
+    ($Env:Path).Split(";")
 }
 
 Remove-Alias -Name ls
@@ -83,33 +86,36 @@ function ls($target) {
 
 Set-Alias ll Get-ChildItem
 
+Set-Alias ctud  ConvertTo-UnixDate
+Set-Alias cfud  ConvertFrom-UnixDate
+
 Remove-Alias -Name cat
 function cat($target) {
-   bat -pp $target
+    bat -pp $target
 }
 
 function catl($target) {
-   bat --style=grid $target
+    bat --style=grid $target
 }
 
 function Get-Process-For-Port($port) {
-	Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
+    Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
 }
 
 function Get-Serial-Number {
     Get-CimInstance -ClassName Win32_Bios | select-object serialnumber
-  }
+}
 
 
 # From https://stackoverflow.com/questions/894430/creating-hard-and-soft-links-using-powershell
 function ln($target, $link) {
-	New-Item -ItemType SymbolicLink -Path $link -Value $target
+    New-Item -ItemType SymbolicLink -Path $link -Value $target
 }
 
 set-alias new-link ln
 
 function which($name) {
-	Get-Command $name | Select-Object -ExpandProperty Definition
+    Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
 function ssh-proxy {
@@ -121,7 +127,7 @@ function df {
 }
 
 function reboot {
-	shutdown /r /t 0
+    shutdown /r /t 0
 }
 
 
@@ -183,7 +189,7 @@ Register-ArgumentCompleter -CommandName $commands -ScriptBlock {
 
     # Map the command to the appropriate bash completion function.
     $F = switch ($commandAst.CommandElements[0].Value) {
-        {$_ -in "awk", "grep", "head", "less", "ls", "sed", "seq", "tail"} {
+        { $_ -in "awk", "grep", "head", "less", "ls", "sed", "seq", "tail" } {
             "_longopt"
             break
         }
@@ -288,6 +294,9 @@ $ENV:STARSHIP_CACHE = "${env:TEMP}\starship"
 Invoke-Expression (&starship init powershell)
 
 Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
-})
+        $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+        (zoxide init --hook $hook powershell) -join "`n"
+    })
+
+Invoke-Expression (@(gh completion -s powershell) -replace " ''\)$", " ' ')" -join "`n")
+Invoke-Expression (@(kaf completion powershell) -replace " ''\)$", " ' ')" -join "`n")
